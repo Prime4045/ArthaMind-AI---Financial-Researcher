745d2641d004f685fbc2811af6ffd3377b8ff552
Raghav Gupta <optimusprime0456@gmail.com>
2026-06-11 02:54:19 +0530

    chore: remove temp build script

--- __git_commit.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d "e:\Projects\Financial Research Automation"
-git add -A
-git commit -m "chore: clean project structure - remove duplicate and cache files"
-git push origin main
-echo Done!
